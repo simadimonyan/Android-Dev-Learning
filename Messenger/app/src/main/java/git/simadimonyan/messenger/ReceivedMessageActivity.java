55b409b5cbe31a0740fe630bf2e694f15586ad16
Dima Simonyan <simadimonyan@gmail.com>
2022-02-17 11:47:48 +0300

    Messenger: First Activity

---
+++ Messenger/app/src/main/java/git/simadimonyan/messenger/ReceivedMessageActivity.java
@@ -0,0 +1,14 @@
+package git.simadimonyan.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReceivedMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_received_message);
+    }
+}
